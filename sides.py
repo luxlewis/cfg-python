4a0e143a63eeccb95c237317020dcfebed9d445c
luxlewis <51483735+luxlewis@users.noreply.github.com>
2020-04-22 10:58:29 +0100

    Delete sides.py

--- sides.py
+++
@@ -1,9 +0,0 @@
-import turtle
-sides = int(input('Number of sides: '))
-angle = 360 / sides
-side_length = 60
-for shape in range(sides):
-    turtle.forward(side_length)
-    turtle.right(angle)
-
-turtle.done()
