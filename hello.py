001124cd903f808cfc5ba173a0b592ee24d07f35
luxlewis <51483735+luxlewis@users.noreply.github.com>
2020-04-22 10:57:03 +0100

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-print('Hello, world!')
-print('I hope it stops raining soon')
-print('The rain in spain stays mainly in the plain')
-print('Brass monkey, that funky monkey')
-print('I like candy')
-
-
-
-
